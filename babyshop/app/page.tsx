13ef2c49e70979a21284f68398200c07d627007e
Pirov Vazirdzhon <vazirpirov15@gmail.com>
2025-08-04 12:15:05 +0900

    main-view

---
+++ babyshop/app/page.tsx
@@ -0,0 +1,8 @@
+// src/app/page.tsx
+export default function Home() {
+  return (
+    <main className="min-h-screen flex items-center justify-center bg-pink-50">
+      <h1 className="text-4xl font-bold text-pink-600">Welcome to BabyShop 👶</h1>
+    </main>
+  )
+}
